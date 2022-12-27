b8432cffe64f8b4a34940f4fec89c98fa1754453
Frans bachtiar <75872210+fransbachtiar24@users.noreply.github.com>
2022-12-27 20:11:00 +0700

    add ticket and fixing bug mybooking

--- src/pages/detailTicket.jsx
+++ src/pages/detailTicket.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../component/Navbar2";
 import Footer from "../component/Footer";
+import { Space, Spin } from "antd";
 import Logo from "../assets/img-plane/Dayss.png";
 import Logo1 from "../assets/img-plane/Day1.png";
 import { useDispatch, useSelector } from "react-redux";
-
 import "../assets/css/home.css";
 import { useParams } from "react-router-dom";
 import { getDetailTicket } from "../redux/actions/transaksiActions";
+import html2canvas from "html2canvas";
+import { saveAs } from "file-saver";
 import axios from "axios";
 
 function DetailTicketPesanan() {
   const dispatch = useDispatch();
   const [data, setDatas] = useState(null);
   const [loading, setLoading] = useState(true);
-
   const { id } = useParams();
 
   const detailPesanan = useSelector((state) => {
@@ -63,8 +64,46 @@ function DetailTicketPesanan() {
   }
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="container">
+        <div className="text-center">
+          <Space
+            direction="vertical"
+            style={{
+              width: "100%",
+            }}
+          >
+            <Space>
+              <Spin size="large">
+                <div className="content" />
+              </Spin>
+            </Space>
+          </Space>
+        </div>
+      </div>
+    );
   }
+
+  // hanlde format RP
+  const rupiah = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(number);
+  };
+
+  const handleDownload = () => {
+    // Ambil element dengan id "my-element"
+    const element = document.getElementById("bg-ticket");
+
+    // Ubah element ke canvas
+    html2canvas(element).then((canvas) => {
+      // Buat file image dan simpan ke komputer pengguna
+      canvas.toBlob((blob) => {
+        saveAs(blob, "my-image.png");
+      });
+    });
+  };
 
   return (
     <React.Fragment>
@@ -87,65 +126,102 @@ function DetailTicketPesanan() {
               </div>
               {/* content */}
               <div className="mt-5 py-3">
-                <div className="container">
-                  <div className="card-body bg-pesanan">
-                    <div className="text-center">
-                      <h4>{data.product.type}</h4>
-                      <hr />
+                <div className="container text-white">
+                  <div id="bg-ticket" className="card-body bg-pesanan">
+                    <div className="text-center bg-eco text-white mt-5">
+                      <h4>
+                        {data.product.type} | {data.product.gate}
+                      </h4>
                     </div>
-
-                    <div className="row ms-5">
-                      <div className="col-lg-3 col-sm-6 col-xs-12 ">
-                        {/* <h4>{detailPesanan.product.terminal}</h4> */}
+                    <div className="row text-center mt-3 py-2">
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ticket-fontsy">
+                        <span>
+                          <h1>Kode</h1>
+                        </span>
+                        <h1>{data.product.code}</h1>
                       </div>
-                      <div className="col-lg-3 col-sm-6 col-xs-12 ">
-                        {/* <h4>{detailPesanan.product.code}</h4> */}
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ticket-fontsy">
+                        <span>
+                          <h1>terminal</h1>
+                        </span>
+                        <h1>{data.product.terminal}</h1>
                       </div>
-                      <div className="col-lg-3 col-sm-6 col-xs-12 ">
-                        {/* <h4>{detailPesanan.total_passenger}</h4> */}
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ticket-fontsy">
+                        <span>
+                          <h1>perjalanan</h1>
+                        </span>
+                        <h1>{convertNumberToTime(data.product.estimation)}</h1>
                       </div>
-                      <div className="col-lg-3 col-sm-6 col-xs-12 ">
-                        <h4>
-                          {
-                            convertNumberToTime()
-                            // detailPesanan.product.estimation
-                          }
-                        </h4>
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ticket-fontsy">
+                        <span>
+                          <h1>penumpang</h1>
+                        </span>
+                        <h1>{data.total_passenger}</h1>
+                      </div>
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ">
+                        <h1>
+                          {data.is_paid ? (
+                            <h1 className="text-success">Lunas</h1>
+                          ) : (
+                            "tidak lunas"
+                          )}
+                        </h1>
+                      </div>
+                      <div className="col-lg-4 col-sm-6 col-xs-12 ticket-fontsy">
+                        <span>
+                          <h1>harga</h1>
+                        </span>
+                        <h1>{rupiah(data.total_order)}</h1>
                       </div>
                     </div>
                   </div>
-                  <div className="ms-5 mt-3">
+                  <div className="ms-5 mt-3 text-dark notes-siterbang">
                     <strong>*Note</strong>
                     <ul>
-                      <li>PENUMPANG SATU</li>
-                      <li>ahhah</li>
+                      <li>Tidak Boleh Membawa Senjata Tajam di atas pesawat</li>
+                      <li>Dilarang Membawa Narkoba</li>
+                      <li>Dilarang Menyalakan HP saat di atas pesawat</li>
                     </ul>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mt-5 ms-5 mb-5">
-              <h4>Detail Penumpang</h4>
-              <table className="table table-bordered">
-                <thead>
-                  <tr>
-                    <th>NIK</th>
-                    <th>Nama</th>
-                    <th>Phone</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {detailPesanan?.map((item) => (
+            <div className="container mt-5 mb-5">
+              <h4 className="detail-penumpang">Detail Penumpang</h4>
+              <div className="table-responsive">
+                <table className="table table-hover table-bordered">
+                  <thead className=" table-pesanan">
                     <tr>
-                      <td>{item.nik}</td>
+                      <th>No</th>
+                      <th>NIK</th>
+                      <th>Nomor Kursi</th>
+                      <th>Nama Lengkap</th>
+                      <th>Nomor Telepon</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="text-center">
+                    {detailPesanan.map((item, index) => (
+                      <tr>
+                        <td key={item.id}>{index + 1}</td>
+                        <td key={index}>{item.nik}</td>
+                        <td key={index}>{item.seat_number}</td>
+                        <td key={index}>{item.passenger_name}</td>
+                        <td key={index}>{item.passenger_phone}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="text-center">
+                <button className="btn btn-success" onClick={handleDownload}>
+                  Download Ticket
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <Footer />
     </React.Fragment>
   );
